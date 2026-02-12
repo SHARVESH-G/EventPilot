df6bdb7f98619444b0c7c238976415104ea18177
SHARVESH-G <sharveshg2405@gmail.com>
2026-02-12 15:05:36 +0530

    User Provider

---
+++ frontend/src/utils/userProvider.jsx
@@ -0,0 +1,62 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import axiosInstance from "../utils/axiosInstance";
+import { saveItem, retrieveItem, deleteItem } from "../utils/localStorageMethods";
+
+const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchUser = async (email) => {
+    try {
+      const { data } = await axiosInstance.get(`/api/auth/user/${email}`);
+      setUser(data.user);
+    } catch (err) {
+      console.log("User fetch failed", err);
+      deleteItem("Email");
+      setUser(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+  useEffect(() => {
+    const email = retrieveItem("Email");
+
+    if (email) {
+      fetchUser(email);
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+
+  const login = (userData) => {
+    saveItem("Email", userData.email);
+    setUser(userData);
+  };
+
+
+  const logout = () => {
+    deleteItem("Email");
+    setUser(null);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+        login,
+        logout,
+        loading,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
